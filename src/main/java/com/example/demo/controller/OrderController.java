e6e07cf46e00248c7a54cbb6f58d3f04223f71f9
villageworks <villageworks@gmail.com>
2023-03-27 16:45:34 +0900

    implement the feature of confirm customer information

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Customer;
 
 @Controller
 public class OrderController {
@@ -13,6 +18,26 @@ public class OrderController {
 	@GetMapping("/order")
 	public String index() {
 		return "customerForm";
+	}
+	
+	/**
+	 * 顧客情報入力確認画面を表示する
+	 * @return 顧客呪法確認画面ファイルベース名
+	 */
+	@PostMapping("/order/confirm")
+	public String confirm(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			Model model) {
+		
+		// Customerクラスをインスタンス化
+		Customer customer = new Customer(name, address, tel, email);
+		// リクエストスコープに登録
+		model.addAttribute("customer", customer);
+		// 確認画面に遷移
+		return "orderConfirm";
 	}
 
 }
